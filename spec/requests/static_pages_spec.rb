ebf1700fadf767f27b4da1e53e70c6aa09179f9e
kurriking@gmail.com <kurriking@gmail.com>
2015-01-31 22:59:18 -0500

    Finish layouts and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,14 +4,12 @@ describe "StaticPages" do
 
   subject {page}
 
-  let(:base_title) {"Rails Tutorial Sampler App"}
-
   describe "Home Page" do
   
   before { visit root_path }
 
    it {should have_content('Sampler App')}
-   it {should have_title("#{base_title}")}
+   it {should have_title(full_title(nil.to_s))}
    it {should_not have_title('| Home')}
   end
 
@@ -20,7 +18,7 @@ describe "StaticPages" do
 before { visit help_path }
 
    it {should have_content('Help')}
-   it {should have_title("#{base_title} | Help")}
+   it {should have_title(full_title('Help'))}
   end
 
   describe "About Page" do
@@ -29,7 +27,7 @@ before {visit about_path}
 
 
     it {should have_content('About Us')}
-    it {should have_title("#{base_title} | About Us")}
+    it {should have_title(full_title("About Us"))}
   end
 
   describe "Contact Us" do
@@ -37,6 +35,6 @@ before {visit about_path}
 before {visit contact_path}
 
     it {should have_content('Contact Us')}
-    it {should have_title("#{base_title} | Contact")}
+    it {should have_title(full_title("Contact"))}
   end
 end
