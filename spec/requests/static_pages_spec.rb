facb2ba357aa54eb512f48cf275a7c5f4e5b2b0c
kurriking@gmail.com <kurriking@gmail.com>
2015-02-04 23:39:36 -0500

    created model user

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,37 +4,66 @@ describe "StaticPages" do
 
   subject {page}
 
+  shared_examples_for "all static pages" do
+    it {should have_selector('h1', text: heading)}
+    it {should have_title(full_title(page_title))}
+  end
+
+
   describe "Home Page" do
   
   before { visit root_path }
+  let(:heading) {"Sampler App"}
+  let(:page_title) {nil.to_s}
 
-   it {should have_content('Sampler App')}
-   it {should have_title(full_title(nil.to_s))}
+
+   it_should_behave_like "all static pages"
    it {should_not have_title('| Home')}
   end
 
   describe "Help Page" do
 
-before { visit help_path }
+  before { visit help_path }
+  let(:heading) {"Help"}
+  let(:page_title) {"Help"}
 
-   it {should have_content('Help')}
-   it {should have_title(full_title('Help'))}
+   it_should_behave_like "all static pages"
   end
 
   describe "About Page" do
 
-before {visit about_path}
+  before {visit about_path}
+  let(:heading) {"About Us"}
+  let(:page_title) {"About Us"}
 
-
-    it {should have_content('About Us')}
-    it {should have_title(full_title("About Us"))}
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact Us" do
 
-before {visit contact_path}
+  before {visit contact_path}
+  let(:heading) {"Contact Us"}
+  let(:page_title) {"Contact"}
 
-    it {should have_content('Contact Us')}
-    it {should have_title(full_title("Contact"))}
+    it_should_behave_like "all static pages"
+  end
+
+  it "describes link routing" do
+    visit root_path
+
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+    click_link "Home"
+    expect(page).to have_title(full_title(''))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "sampler app"
+    expect(page).to have_title(full_title(nil.to_s))
+    click_link "Sign up NOW!"
+    expect(page).to have_title(full_title('Sign Up'))
+    click_link "Sign up"
+    expect(page).to have_selector('h1', text: "Sign Up")
   end
 end
