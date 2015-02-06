1d195cd001950eb57d4a5d6801c98efb4fba694e
kurriking@gmail.com <kurriking@gmail.com>
2015-02-06 00:46:46 -0500

    User model and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ActiveRecord::Base
+	before_save {self.email = email.downcase}
+
+	validates :name, presence: true
+	validates :email, presence: true
+
+	validates :name, length:{maximum: 50}
+
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, format:{with: VALID_EMAIL_REGEX}
+
+    validates :email, uniqueness: {case_sensitive: false}
+
+    has_secure_password
+    validates :password, length: {minimum: 6}
 end
